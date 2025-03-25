798c4130817e74c390d6d7b802b6011d147f00fd
Magzhan Dautbekov <magzhandautbekov@gmail.com>
2025-03-25 22:38:11 +0500

    auth check added

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -6,29 +6,42 @@ import {ProjectCreateComponent} from './pages/project-create/project-create.comp
 import {ProjectComponent} from './pages/project/project.component';
 import {ProjectCatalogComponent} from './pages/project-catalog/project-catalog.component';
 import {ProfileComponent} from './pages/profile/profile.component';
-import {RegisterPageComponent} from './pages/register-page/register-page.component';
 import {LoginPageComponent} from './pages/login-page/login-page.component';
 import {HelpComponent} from './pages/help/help.component';
 import {BlogComponent} from './pages/blog/blog.component';
 import {ContactComponent} from './pages/contact/contact.component';
+import {NoAuthGuard} from './auth/guards/noAuth.guard';
+import {AuthGuard} from './auth/guards/auth.guard';
 
 export const routes: Routes = [
   {
     path: '',
     component: LayoutComponent,
+    canActivate: [NoAuthGuard],
     children: [
-      { path: '', component: HomeComponent },
-      { path: 'about', component: AboutComponent },
+      {path: '',component: HomeComponent},
+      {path: 'about',component: AboutComponent},
+      {path: 'help',component: HelpComponent},
+      {path: 'blog', component: BlogComponent },
+      {path: 'contact', component: ContactComponent },
+    ],
+
+  },
+  {
+    path: '',
+    component: LayoutComponent,
+    canActivate: [AuthGuard],
+    children: [
       { path: 'profile', component: ProfileComponent },
       { path: 'create-project', component: ProjectCreateComponent },
       { path: 'project/:id', component: ProjectComponent },
       { path: 'catalog', component: ProjectCatalogComponent },
-      { path: 'help', component: HelpComponent },
-      { path: 'blog', component: BlogComponent },
-      { path: 'contact', component: ContactComponent },
     ],
 
   },
-  { path: 'register', component: RegisterPageComponent },
-  { path: 'login', component: LoginPageComponent },
+  {
+    path: 'login',
+    component: LoginPageComponent,
+    canActivate: [NoAuthGuard]
+  },
 ];

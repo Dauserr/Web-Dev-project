872a8c6db6282b9e4429a872dcb79feabf74f50b
Magzhan Dautbekov <magzhandautbekov@gmail.com>
2025-04-02 16:45:40 +0500

    IndexError exception added to the user_profile service

--- backend/user_profile/views.py
+++ backend/user_profile/views.py
@@ -5,7 +5,7 @@ from django.forms import model_to_dict
 from django.http import JsonResponse
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
-from jwt import ExpiredSignatureError
+from jwt import ExpiredSignatureError, DecodeError
 
 from auth_service.models import Users
 from backend_service.settings import JWT_SECRET_KEY
@@ -15,10 +15,11 @@ from backend_service.settings import JWT_SECRET_KEY
 
 @csrf_exempt
 def getUserData(request):
+    print('token:', f'{len(request.headers.get("Authorization"))}')
     if request.method == 'GET':
         try:
             token = request.headers.get('Authorization')
-            if token and token.startswith('Bearer '):
+            if token and token.startswith('Bearer'):
                 token = token.split(' ')[1]
                 decoded = jwt.decode(token, JWT_SECRET_KEY, algorithms=['HS256'])
                 user = Users.objects.filter(user_name=decoded.get('username')).first()
@@ -27,8 +28,12 @@ def getUserData(request):
                 return JsonResponse({"data": model_to_dict(user, fields=['user_name', 'user_fullName', 'user_phoneNumber', 'user_description']), "response_code": "SUCCESS"}, status=200)
             else:
                 return JsonResponse({"response_code": "WRONG_TOKEN_FORMAT"}, status=407)
+        except IndexError:
+            return JsonResponse({"response_code": "TOKEN_IS_EMPTY"}, status=402)
         except ExpiredSignatureError:
             return JsonResponse({"response_code": "TOKEN_EXPIRED"}, status=401)
+        except DecodeError:
+            return JsonResponse({"response_code": "TOKEN_DECODE_ERROR"}, status=410)
         except json.JSONDecoder:
             return JsonResponse({"error": "Некорректный JSON", "response_code": "WRONG_JSON"}, status=400)
 

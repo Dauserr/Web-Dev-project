bf9cefe6b7bd48384d546da3f379e2a0b4ca424b
olzhassembinov <156879774+olzhassembinov@users.noreply.github.com>
2025-04-05 12:35:14 +0500

    Footer+Home добавил ссылки

--- frontend/src/app/components/footer/footer.component.ts
+++ frontend/src/app/components/footer/footer.component.ts
@@ -1,6 +1,7 @@
 import {Component, inject} from '@angular/core';
 import {TranslocoModule, TranslocoService} from '@ngneat/transloco';
 import {NgForOf} from '@angular/common';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-footer',
@@ -13,20 +14,47 @@ export class FooterComponent {
   private translocoService = inject(TranslocoService);
   t = (key: string) => this.translocoService.translate(key);
 
+  constructor(private _router:Router) {}
+
   footerItems = [
     {
       columnHeadDefinition:'about_platform',
-      columnItems:['about_us','how_it_works','rules','security']
+      columnItems:[
+        {name:'about_us', link:'../about'},
+        {name:'how_it_works', link:'../about'},
+        {name:'rules', link:'../about'},
+        {name:'security', link:'../about'}
+      ]
     },{
       columnHeadDefinition:'contacts',
-      columnItems:['email','phone','address']
+      columnItems:[
+        {name:'email', link:'#'},
+        {name:'phone', link:'#'},
+        {name:'address', link:'#'}
+      ]
     },{
       columnHeadDefinition:'social_networks',
-      columnItems:['Facebook','Instagram','Twitter','LinkedIn']
+      columnItems:[
+        {name:'Facebook', link:'https://facebook.com'},
+        {name:'Instagram', link:'https://instagram.com'},
+        {name:'Twitter', link:'https://twitter.com'},
+        {name:'LinkedIn', link:'https://linkedin.com'}
+      ]
     },{
       columnHeadDefinition:'legal_information',
-      columnItems:['terms_of_use','privacy_policy','platform_rules']
+      columnItems:[
+        {name:'terms_of_use', link:'../about'},
+        {name:'privacy_policy', link:'../about'},
+        {name:'platform_rules', link:'../about'}
+      ]
     },
   ]
 
+  navigateByUrl(url:string){
+    if (url.startsWith('http')) {
+      window.open(url, '_blank');
+    } else {
+      this._router.navigate([url]);
+    }
+  }
 }

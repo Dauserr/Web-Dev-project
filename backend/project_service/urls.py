aab39f26b8725129ef1b35a3c703a545847b929a
Magzhan Dautbekov <magzhandautbekov@gmail.com>
2025-04-07 17:40:05 +0500

    filter additional features backend added

--- backend/project_service/urls.py
+++ backend/project_service/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import project_catalogue, project_detail, create_project
+from .views import project_catalogue, project_detail, create_project, getCatalog
 
 urlpatterns = [
     path("projects/", project_catalogue, name="project_catalogue"),
     path("projects/<int:project_id>/", project_detail, name="project_detail"),
     path("projects/create", create_project, name="create_project"),
+    path("catalog", getCatalog, name="getCatalog")
 ]

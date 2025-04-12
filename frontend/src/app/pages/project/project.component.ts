3a0a98a1041f6d088c38caeb3d11e2689e163a8f
Magzhan Dautbekov <magzhandautbekov@gmail.com>
2025-04-12 21:46:20 +0500

    project page fixes

--- frontend/src/app/pages/project/project.component.ts
+++ frontend/src/app/pages/project/project.component.ts
@@ -3,38 +3,47 @@ import { CommonModule } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
 import { ApiUrlsService } from '../../services/api-urls.service';
 import { TranslocoService } from '@ngneat/transloco';
+import {MatProgressSpinner} from '@angular/material/progress-spinner';
 
 @Component({
   selector: 'app-project',
   templateUrl: './project.component.html',
   styleUrls: ['./project.component.css'],
-  imports: [CommonModule]
+  standalone:true,
+  imports: [CommonModule, MatProgressSpinner]
 })
 export class ProjectComponent implements OnInit {
   private translocoService = inject(TranslocoService);
   t = (key: string) => this.translocoService.translate(key);
   project: any;
-
+  isLoadingPage:boolean = false
   constructor(
     private route: ActivatedRoute, // для получения параметра ID из URL
     private apiUrlsService: ApiUrlsService // сервис для работы с API
   ) {}
   ngOnInit(): void {
-    // Получаем ID из URL
     const projectId = this.route.snapshot.paramMap.get('id');
-    if (projectId) {
-      this.loadProjectDetail(projectId);
-    }
+    this.loadProjectDetail(projectId || '');
   }
 
   loadProjectDetail(projectId: string): void {
-    this.apiUrlsService.getProjectDetail(projectId).subscribe(
-      (data) => {
+    document.body.style.overflowY = 'hidden'
+    this.isLoadingPage = true
+    const extractedId = projectId.split('_')[2]
+    this.apiUrlsService.getProjectDetail(extractedId).subscribe({
+      next:(data) => {
         this.project = data;
+        this.isLoadingPage = false
       },
-      (error) => {
+      error:(error) => {
         console.error('Ошибка при загрузке данных проекта', error);
+        this.isLoadingPage = false
+      },
+      complete:() => {
+        this.isLoadingPage = false
+        document.body.style.overflowY = 'auto'
       }
+    }
     );
   }
 }

f05429a4a81435a745c69e603f8ec4268f99bc90
Dauserr <dauser200606@gmail.com>
2025-04-25 18:10:14 +0500

    blog -1 fbv +1 cbv

--- backend/blog_service/views.py
+++ backend/blog_service/views.py
@@ -1,10 +1,10 @@
-from rest_framework.decorators import api_view
+from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import News
 from .serializers import NewsSerializer
 
-@api_view(['GET'])
-def get_news(request):
-    news = News.objects.all()
-    serializer = NewsSerializer(news, many=True)
-    return Response(serializer.data)
+class NewsListView(APIView):
+    def get(self, request):
+        news = News.objects.all()
+        serializer = NewsSerializer(news, many=True)
+        return Response(serializer.data)

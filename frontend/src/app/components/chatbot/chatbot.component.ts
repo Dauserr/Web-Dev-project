c0392458685f97310583c8c836b6c6bf350d9aa4
Magzhan Dautbekov <magzhandautbekov@gmail.com>
2025-05-05 17:49:12 +0500

    ui chatbot finished

--- frontend/src/app/components/chatbot/chatbot.component.ts
+++ frontend/src/app/components/chatbot/chatbot.component.ts
@@ -1,19 +1,65 @@
 import { Component } from '@angular/core';
 import { FontAwesomeModule} from '@fortawesome/angular-fontawesome';
-import {faPersonCircleQuestion, faQuestion, faQuestionCircle} from '@fortawesome/free-solid-svg-icons';
+import {
+  faMessage,
+  faPaperPlane,
+  faPersonCircleQuestion,
+  faQuestion,
+  faQuestionCircle
+} from '@fortawesome/free-solid-svg-icons';
+import {MatCard} from '@angular/material/card';
+import {FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
+import {NgForOf, NgIf} from '@angular/common';
+
+interface Message{
+  title:string,
+  sendTime:string
+}
 
 @Component({
   selector: 'app-chatbot',
   imports: [
-    FontAwesomeModule
+    FontAwesomeModule,
+    MatCard,
+    FormsModule,
+    ReactiveFormsModule,
+    NgIf,
+    NgForOf
   ],
   standalone:true,
   templateUrl: './chatbot.component.html',
   styleUrl: './chatbot.component.css'
 })
 export class ChatbotComponent {
+  showChatbot = false;
+  chatQuestionsForm!:FormGroup
+  hasMessages:boolean = false
+  messages : Message[] = []
+  constructor(private fb:FormBuilder) {
+    this.chatQuestionsForm = this.fb.group({
+      message:['',[Validators.required]]
+    })
+  }
+
+  sendMessage(){
+    const currentDate = new Date()
+    const objToStore = {
+      title:this.chatQuestionsForm.value.message,
+      sendTime:currentDate.toLocaleTimeString([],{
+        hour:'2-digit',
+        minute:'2-digit',
+        hour12:false,
+      })
+    }
+    this.messages.push(objToStore)
+    this.hasMessages = true
+    this.chatQuestionsForm.patchValue({message:''})
+  }
+
 
   protected readonly faQuestion = faQuestion;
   protected readonly faPersonCircleQuestion = faPersonCircleQuestion;
   protected readonly faQuestionCircle = faQuestionCircle;
+  protected readonly faMessage = faMessage;
+  protected readonly faPaperPlane = faPaperPlane;
 }
